66e1deb7e11fdcdecc16219d76479cb8dce5015a
Prashant kumar <114512068+Prashant-kr01@users.noreply.github.com>
2025-04-04 14:47:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 let count = 0;
-document.getElementById("btn").addEventListener("click", function(){
+document.getElementById("incrementBtn").addEventListener("click", function(){
 	alert("Previous count: " + count);
 	count++;
 	document.getElementById("counter").textContent = count;
