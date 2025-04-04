fbe5d46587c932206457cca71eed3a643f8144c6
Prashant kumar <114512068+Prashant-kr01@users.noreply.github.com>
2025-04-04 14:43:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let count = 0;
+document.getElementById("btn").addEventListener("click", function(){
+	alert("Previous count: " + count);
+	count++;
+	document.getElementById("counter").textContent = count;
+});
